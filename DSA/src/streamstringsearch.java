8b35f1d68868e34618cabfaba4cf333922642998
savinshetty <savinshetty@localhost>
2022-07-27 00:27:17 +0530

    first dsa commit

--- DSA/src/streamstringsearch.java
+++ DSA/src/streamstringsearch.java
@@ -1,6 +1,5 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class streamstringsearch {
@@ -8,7 +7,7 @@ public class streamstringsearch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String[] mySary	= {"Hello","Hey","Hi","Hi","World"};
+		String[] mySary	= {"Art","gallery","Hi","Hi","abstract"};
 		List<String> mSalst = Arrays.asList(mySary);
 		mSalst.stream().distinct().collect(Collectors.toList()).
 		forEach(s->System.out.println(s));
